ffacee8cd06a5dce171b09fc2474938173547f91
Michael Risser <rissercode@gmail.com>
2022-05-19 13:50:53 -0500

    Added a date format.

--- src/components/Layouts/OrderDataTable.js
+++ src/components/Layouts/OrderDataTable.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
+import { format } from 'date-fns';
 import { DataGrid } from "@mui/x-data-grid";
 import { Link } from "@mui/material";
 
 const columns = [
     {field: 'orderId', headerName: 'Order Number', width: 125, headerAlign: 'center', 
     renderCell:rowData=><Link href={`https://localhost:7143/api/order/${rowData.id}`} target="_blank">{rowData.id}</Link> },
-    {field: 'orderDate', headerName: 'Order Date', headerAlign: 'center', width: 150},
+    {field: 'orderDate', headerName: 'Order Date', headerAlign: 'center', width: 150, format: 'dd/mm/yyyy'},
     {field: 'height', headerName: 'Heigh', headerAlign: 'center'},
     {field: 'width', headerName: 'Width', headerAlign: 'center'},
     {field: 'cost', headerName: 'Cost', headerAlign: 'center'},
@@ -20,6 +21,7 @@ function OrderDataTable() {
     useEffect(() => {
         fetch("https://localhost:7143/api/customerorders/1")
         .then((data) => data.json())
+        .then(format())
         .then((data) => setTableData(data))
     })
 
