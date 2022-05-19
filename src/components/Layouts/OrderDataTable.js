ed56fd9e82a1535733d4f9ad457c2815f455c47f
Michael Risser <rissercode@gmail.com>
2022-05-19 14:00:46 -0500

    Updated the date format.

--- src/components/Layouts/OrderDataTable.js
+++ src/components/Layouts/OrderDataTable.js
@@ -8,7 +8,7 @@ import { Link } from "@mui/material";
 const columns = [
     {field: 'orderId', headerName: 'Order Number', width: 125, headerAlign: 'center', 
     renderCell:rowData=><Link href={`https://localhost:7143/api/order/${rowData.id}`} target="_blank">{rowData.id}</Link> },
-    {field: 'orderDate', headerName: 'Order Date', headerAlign: 'center', width: 150, format: 'dd/mm/yyyy'},
+    {field: 'orderDate', headerName: 'Order Date', headerAlign: 'center', width: 150, format: 'mm/dd/yyyy'},
     {field: 'height', headerName: 'Heigh', headerAlign: 'center'},
     {field: 'width', headerName: 'Width', headerAlign: 'center'},
     {field: 'cost', headerName: 'Cost', headerAlign: 'center'},
